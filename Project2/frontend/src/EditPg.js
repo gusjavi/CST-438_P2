e084f90d5906d48e6e7e125691acfc92e90c202c
gusjavi <gujavier@csumb.edu>
2025-02-24 22:01:11 -0800

    The basic template for landing, tierlist, and edit pages, have more to it , although not crazy work, still need to wait for db

--- Project2/frontend/src/EditPg.js
+++ Project2/frontend/src/EditPg.js
@@ -1,20 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
-import "./styles.css";
+import "./innerPages.css";
 
 function EditPg() {
     const navigate = useNavigate();
 
+    const [formData, setFormData] = useState({ username: "", password: "" });
+    const [error, setError] = useState("");
+
+    function handleChange(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        navigate("/landing");
+    }
+     function deleteAccount(){
+         {/*waiting for db*/}
+     }
+    function deleteInfo(){
+        {/*waiting for db*/}
+    }
+
+
     return (
-        <div className="container">
+        <div>
             <h1>Welcome to the Edit</h1>
-            <button onClick={() => navigate("/landing")} className="btn">
-                Go to Home
-            </button>
-            <button onClick={() => navigate("/tier")} className="btn">
-                Go to Tier
-            </button>
+
+            <div className="container">
+                <button onClick={() => navigate("/landing")} className="btn">
+                    Go to Home
+                </button>
+                <button onClick={() => navigate("/tier")} className="btn">
+                    Go to Tier
+                </button>
+            </div>
+            <div className="container2">
+            <form onSubmit={handleSubmit} className="form">
+                <input type="text" name="username" placeholder="Username or Email" value={formData.username} onChange={handleChange} required className="input" />
+                <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required className="input" />
+                {error && <p className="error">{error}</p>}
+                <button type="submit" className="btn">Update Profile</button>
+            </form> {/* can pull the username and password here*/}
+            </div>
+            <div className="container3">
+                <button onClick={() => deleteAccount()} className="btn">
+                    Delete Profile
+                </button>
+                <button onClick={() => deleteInfo()} className="btn">
+                    Delete Info
+                </button>
+            </div>
         </div>
+
     );
 }
 
--- Project2/frontend/src/LandingPg.js
+++ Project2/frontend/src/LandingPg.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./styles.css";
+import "./innerPages.css";
 
 function LandingPg() {
     const navigate = useNavigate();
     return (
-        <div className="container">
+        <div>
             <h1>Welcome to the Landing Page</h1>
-            <button onClick={() => navigate("/tier")} className="btn">
-                Go to TierList
-            </button>
-            <button onClick={() => navigate("/edit")} className="btn">
-                Edit Account
-            </button>
+
+            <div className="container">
+                <button onClick={() => navigate("/tier")} className="btn">
+                    Go to TierList
+                </button>
+                <button onClick={() => navigate("/edit")} className="btn">
+                    Edit Account
+                </button>
+            </div>
+
+            <h2>Hello USERNAME !</h2>{/*placeholder for now, maybe session used here*/}
+
+            <h2>Tier List of the Week placeholder !</h2>{/*placeholder for now, maybe session used here*/}
+
+
         </div>
+
     );
 }
 
