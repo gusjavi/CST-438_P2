47569d8bda8ed77545fd7f7bbb2b82693b56ad20
gusjavi <157551742+gusjavi@users.noreply.github.com>
2025-03-16 19:15:04 -0700

    Update FirebaseConfigController.java

--- Project2/backend/src/main/java/org/tierlist/project02backend/controller/FirebaseConfigController.java
+++ Project2/backend/src/main/java/org/tierlist/project02backend/controller/FirebaseConfigController.java
@@ -12,12 +12,6 @@ public class FirebaseConfigController {
     @GetMapping("/firebase-config")
     public Map<String, String> getFirebaseConfig() {
         return Map.of(
-                "apiKey", "AIzaSyDRUhjpENDGnVxcWzf7GZEXYEI620m0FUc",
-                "authDomain", "autho-8946f.firebaseapp.com",
-                "projectId", "autho-8946f",
-                "storageBucket", "autho-8946f.appspot.com",
-                "messagingSenderId", "812368438765",
-                "appId", "1:812368438765:web:0daa5d1f2b9ef8db873e4"
-        );
+                
     }
 }
